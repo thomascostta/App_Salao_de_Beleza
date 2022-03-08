f4490972660a29f758767bfcb7b5f7fb11aa6bc6
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-08 18:02:42 -0300

    settings

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ import {AppRegistry} from 'react-native';
 import Home from './src/pages/Home';
 import {name as appName} from './app.json';
 
-import { Provider } from 'react-redux';
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
 import store from './src/store';
+import { fonts } from './src/styles/theme.json'
+
+const theme = {
+    ...DefaultTheme,
+    fonts: configureFonts({
+        ios: fonts,
+        android: fonts ,
+    })
+}
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <Home />
-        </Provider>
+        <StoreProvider store={store}>
+            <PaperProvider theme={theme}>
+                <Home />
+            </PaperProvider>
+        </StoreProvider>
     )
 }
 
