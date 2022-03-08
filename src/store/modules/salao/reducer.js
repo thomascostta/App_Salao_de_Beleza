70f906d018a5e2b53eb915a83154c08680d5f4e9
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-08 12:14:13 -0300

    create reducer of Redux

--- src/store/modules/salao/reducer.js
+++ src/store/modules/salao/reducer.js
@@ -3,22 +3,30 @@ import produce from 'immer';
 import consts from '../../../consts';
 
 const INITIAL_STATE = {
-    salao: {},
-    servicos: [],
-    agenda: [],
-    colaboradores: [],
-    agendamento: {
-      clienteId: consts.clienteId,
-      salaoId: consts.salaoId,
-      servicoId: null,
-      colaboradorId: null,
-      data: null,
-    },
-    form: {
-      inputFiltro: '',
-      inputFiltroFoco: false,
-      modalEspecialista: false,
-      modalAgendamento: 0,
-      agendamentoLoading: false,
-    },
-  };
+  salao: {},
+  servicos: [],
+  agenda: [],
+  colaboradores: [],
+  agendamento: {
+    clienteId: consts.clienteId,
+    salaoId: consts.salaoId,
+    servicoId: null,
+    colaboradorId: null,
+    data: null,
+  },
+  form: {
+    inputFiltro: '',
+    inputFiltroFoco: false,
+    modalEspecialista: false,
+    modalAgendamento: 0,
+    agendamentoLoading: false,
+  },
+};
+
+
+export default function salao(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
