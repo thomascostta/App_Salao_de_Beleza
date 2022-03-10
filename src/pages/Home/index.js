6342221be094054e951fc7f46cac8d4c3aeb6cc8
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-10 12:38:17 -0300

    settings Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 import Header from '../Header';
+import theme from '../../styles/theme.json';
+import util from '../../util';
 
 const Home = () => {
     return (
         <FlatList
+            stylee={{
+                backgroundColor: util.toAlpha(theme.color.muted, 10)
+            }}
             ListEmptyComponent={Header}
             data={[]}
         />
