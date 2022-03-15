5c1a9da8d105ffb2205821fe08ed6751d27e920e
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-15 18:30:27 -0300

    create ModalSpecialists

---
+++ src/components/ModalScheduling/specialists/modalSpecialists.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Modal from "react-native-simple-modal";
+
+import { Box, Text, Cover, Button, Touchable } from "../../../styles";
+import dataSpecialist from "../../../config/dataSpecialist.json";
+import theme from "../../../styles/theme.json";
+
+const ModalSpecialists = () => {
+  const [borderColor, setBorderColor] = useState();
+  return (
+    <Modal open={true}>
+      <ScrollView>
+        <Box hasPadding direction="column">
+          <Text bold color="dark">
+            Selecione o Especialista
+          </Text>
+          <Text small>Disponíveis em 20/04/21 (Sex) às 11:30</Text>
+          <Box spacing="10px 0 0">
+            {dataSpecialist.results.map((item) => {
+              return (
+                <Touchable
+                  width="70px"
+                  height="70px"
+                  spacing="10px 0 0 0"
+                  direction="column"
+                  align="center"
+                  onPress={() => setBorderColor(item)}
+                >
+                  <Image
+                    style={{
+                      width: 45,
+                      height: 45,
+                      borderRadius: 50,
+                      borderWidth: 4,
+                      borderColor: item === borderColor ? theme.colors.primary : null
+                    }}
+                    source={{ uri: item.picture.medium }}
+                  />
+                  <Text bold small>
+                    {item.name.first}
+                  </Text>
+                </Touchable>
+              );
+            })}
+          </Box>
+        </Box>
+      </ScrollView>
+    </Modal>
+  );
+};
+
+export default ModalSpecialists;
