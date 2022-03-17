5bc4f829730109d89510f7793eaaa502f10cf975
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-17 18:01:34 -0300

    settings

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Linking } from "react-native";
 import {
   Cover,
   GradientView,
@@ -9,10 +9,11 @@ import {
   Box,
   Touchable,
   Button,
-  TextInput
+  TextInput,
 } from "../../styles";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import theme from "../../styles/theme.json";
+import { results } from "../../data/dataHaircut.json";
 
 const Header = () => {
   return (
@@ -40,13 +41,25 @@ const Header = () => {
         width={Dimensions.get("window").width}
       >
         <Box justify="space-between" hasPadding>
-          <Touchable width="65px" align="center" direction="column">
+          <Touchable
+            width="65px"
+            align="center"
+            direction="column"
+            onPress={() => Linking.openURL(`tel:11977223377`)}
+          >
             <Icon name="phone" size={24} color={theme.colors.muted} />
             <Text small spacing="10px 0 0">
               Ligar
             </Text>
           </Touchable>
-          <Touchable width="65px" align="center" direction="column">
+          <Touchable
+            width="65px"
+            align="center"
+            direction="column"
+            onPress={() => {
+              Linking.openURL(`https://goo.gl/maps/ZF1ofYuixFnyuMhi9`);
+            }}
+          >
             <Icon name="map-marker" size={24} color={theme.colors.muted} />
             <Text small spacing="10px 0 0">
               Visitar
@@ -74,8 +87,8 @@ const Header = () => {
         </Box>
       </Box>
       <Box hasPadding direction="column" background="light" spacing="10px 0 0">
-          <Title small>Serviços (2)</Title>
-          <TextInput placeholder="Bora ficar bonito..."></TextInput>
+        <Title small>Serviços - {results.length}</Title>
+        <TextInput placeholder="Bora ficar bonito..."></TextInput>
       </Box>
     </>
   );
