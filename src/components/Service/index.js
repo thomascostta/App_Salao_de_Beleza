203495a3e533f09d04e64d568de7d0e621515be2
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-17 18:02:23 -0300

    settings

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,26 +1,38 @@
 import React from "react";
+import { View, Image } from "react-native";
 import { Text, Touchable, Box, Cover, Title, Button } from "../../styles";
 
-const Service = (key) => {
+
+const Service = ({item}) => {
   return (
-    <Touchable key={key} height="100px" hasPadding align="center" background="light">
-      <Cover image="https://static1.fiquediva.com.br/articles/4/18/69/4/@/144160-o-falso-moicano-no-cabelo-masculino-e-um-article_gallery_portrait-2.jpg" />
-      <Box direction="column">
-        <Text bold color="dark">
-          Corte de cabelo
-        </Text>
-        <Text small>R$ 30,00 • 30 minutos</Text>
-      </Box>
-      <Box>
-        <Button
-          icon="clock-check-outline"
-          background="success"
-          mode="contained"
-        >
-          AGENDAR
-        </Button>
-      </Box>
-    </Touchable>
+      <Touchable
+        key={item.id}
+        height="100px"
+        hasPadding
+        align="center"
+        background="light"
+      >
+        <Image
+          source={{ uri: item.image }}
+          style={{ width: 60, height: 70, borderRadius: 3, borderWidth: 2, marginHorizontal:10 }}
+        />
+        {/* <Cover image="https://static1.fiquediva.com.br/articles/4/18/69/4/@/144160-o-falso-moicano-no-cabelo-masculino-e-um-article_gallery_portrait-2.jpg" /> */}
+        <Box direction="column" hasPadding>
+          <Text bold color="dark">
+            {item.corte}
+          </Text>
+          <Text small>{item.valor} • 30 minutos</Text>
+        </Box>
+        <Box>
+          <Button
+            icon="clock-check-outline"
+            background="success"
+            mode="contained"
+          >
+            AGENDAR
+          </Button>
+        </Box>
+      </Touchable>
   );
 };
 
