13d7383f233809dc19fa28f26385652e291d2167
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-23 10:18:07 -0300

    create dataTime

---
+++ src/data/dataTime.js
@@ -0,0 +1,6 @@
+export const dataTime = [
+  { time: "14:30", id: 1, status: false },
+  { time: "15:30", id: 2, status: false },
+  { time: "16:00", id: 3, status: false },
+  { time: "17:00", id: 4, status: false },
+];
