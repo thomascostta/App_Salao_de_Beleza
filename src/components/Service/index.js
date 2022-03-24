8a0aeb807cd09b9b19f71c2e2dee636d932abbd7
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-24 11:53:32 -0300

    settings

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,38 +1,44 @@
 import React from "react";
-import { View, Image } from "react-native";
-import { Text, Touchable, Box, Cover, Title, Button } from "../../styles";
+import { Image } from "react-native";
+import { Text, Touchable, Box, Button } from "../../styles";
+import moment from "moment";
 
+const Service = ({ services }) => {
 
-const Service = ({item}) => {
+  const currencyFormat = (num) => {
+    return "R$ " + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
   return (
-      <Touchable
-        key={item.id}
-        height="100px"
-        hasPadding
-        align="center"
-        background="light"
-      >
-        <Image
-          source={{ uri: item.image }}
-          style={{ width: 60, height: 70, borderRadius: 3, borderWidth: 2, marginHorizontal:10 }}
-        />
-        {/* <Cover image="https://static1.fiquediva.com.br/articles/4/18/69/4/@/144160-o-falso-moicano-no-cabelo-masculino-e-um-article_gallery_portrait-2.jpg" /> */}
-        <Box direction="column" hasPadding>
-          <Text bold color="dark">
-            {item.corte}
-          </Text>
-          <Text small>{item.valor} • 30 minutos</Text>
-        </Box>
-        <Box>
-          <Button
-            icon="clock-check-outline"
-            background="success"
-            mode="contained"
-          >
-            AGENDAR
-          </Button>
-        </Box>
-      </Touchable>
+    <Touchable height="100px" hasPadding align="center" background="light">
+      <Image
+        source={{ uri: services.image }}
+        style={{
+          width: 60,
+          height: 70,
+          borderRadius: 3,
+          borderWidth: 2,
+          marginHorizontal: 10,
+        }}
+      />
+      <Box direction="column" hasPadding>
+        <Text bold color="dark">
+          {services.corte}
+        </Text>
+        <Text small>
+          {currencyFormat(services.valor)} • {services.time} min
+        </Text>
+      </Box>
+      <Box>
+        <Button
+          icon="clock-check-outline"
+          background="success"
+          mode="contained"
+        >
+          AGENDAR
+        </Button>
+      </Box>
+    </Touchable>
   );
 };
 
