1da8171f39ccfa60ceaa264f05b9b554a9cc4ca3
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-24 11:53:55 -0300

    redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlatList, View, Image } from "react-native";
+import { useSelector } from "react-redux";
 
 import Header from "../../components/Header";
 import theme from "../../styles/theme.json";
@@ -7,12 +8,12 @@ import util from "../../util";
 import Service from "../../components/Service";
 import ModalScheduling from "../../components/ModalScheduling";
 import ModalSpecialists from "../../components/ModalScheduling/specialists/modalSpecialists";
-import { results } from "../../data/dataHaircut.json";
 
 const Home = () => {
-  
+  const { services } = useSelector((state) => state.salao);
+
   const renderItem = ({ item }) => {
-    return <Service item={item} />;
+    return <Service services={item} key={item} />;
   };
 
   return (
@@ -22,7 +23,7 @@ const Home = () => {
           backgroundColor: util.toAlpha(theme.colors.muted, 10),
         }}
         ListHeaderComponent={Header}
-        data={results}
+        data={services}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
       />
