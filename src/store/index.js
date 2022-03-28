fb7b3c08bddab2335d906f26b360d092545c4d1d
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-28 17:47:06 -0300

    settings Redux-Persisit

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
-import Reactotron from '../config/reactotron';
+import rootReducer from "./modules/rootReducer";
+import rootSaga from "./modules/rootSaga";
+import Reactotron from "../config/reactotron";
+import { persistStore, persistReducer } from "redux-persist";
+// import storage from 'redux-persist/lib/storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [];
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(sagaMiddleware), Reactotron.createEnhancer())
+  persistedReducer,
+  compose(applyMiddleware(sagaMiddleware), Reactotron.createEnhancer())
 );
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
