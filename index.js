a206740c6735f3e731f853af51b77080499f610d
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-28 17:46:27 -0300

    settigns Redux-persist

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import Home from './src/pages/Home';
-import { name as appName } from './app.json';
+import React from "react";
+import { AppRegistry } from "react-native";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import {
+  configureFonts,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
+import { name as appName } from "./app.json";
 
-import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import { Provider as StoreProvider } from 'react-redux';
-import store from './src/store';
-import { fonts } from './src/styles/theme.json'
+import Home from "./src/pages/Home";
+import { store, persistor } from "./src/store";
+
+import { fonts } from "./src/styles/theme.json";
 
 const theme = {
-    ...DefaultTheme,
-    fonts: configureFonts({
-        ios: fonts,
-        android: fonts,
-    })
-}
+  ...DefaultTheme,
+  fonts: configureFonts({
+    ios: fonts,
+    android: fonts,
+  }),
+};
 
 const App = () => {
-    return (
-        <StoreProvider store={store}>
-            <PaperProvider theme={theme}>
-                <Home />
-            </PaperProvider>
-        </StoreProvider>
-    )
-}
+  return (
+    <StoreProvider store={store}>
+      <PersistGate persistor={persistor}>
+        <PaperProvider theme={theme}>
+          <Home />
+        </PaperProvider>
+      </PersistGate>
+    </StoreProvider>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => App);
