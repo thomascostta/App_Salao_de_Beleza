78efb2012ceb83ba104ca5adda2d48c1407bab92
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-29 18:04:03 -0300

    update action

--- src/store/modules/salao/actions.js
+++ src/store/modules/salao/actions.js
@@ -1,5 +1,9 @@
-import types from './types';
+import types from "./types";
 
 export function allServices(services) {
-    return { type: types.ALL_SERVICES, services}
+  return { type: types.ALL_SERVICES, services };
+}
+
+export function updateForm(payload) {
+  return { type: types.UPDATE_FORM, payload };
 }
