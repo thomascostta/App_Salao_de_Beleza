760609c3b17f71825a084efffecab478fd27cf2a
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-29 11:52:51 -0300

    update to Reducer

---
+++ src/data/dataHaircut.js
@@ -0,0 +1,50 @@
+export const dataHaircut = [
+  {
+    id: "1",
+    corte: "Bold undercut para cabelos lisos",
+    valor: 35.0,
+    time: 30,
+    image:
+      "https://salaovirtual.org/wp-content/uploads/2016/05/undercut-com-coque.jpg",
+  },
+  {
+    id: "2",
+    corte: "Undercut com fade e cabelo mais longo",
+    valor: 30.0,
+    time: 25,
+    image:
+      "https://salaovirtual.org/wp-content/uploads/2016/05/pomada-barba.jpg",
+  },
+  {
+    id: "3",
+    corte: "Black fade haircut",
+    valor: 20.0,
+    time: 30,
+    image:
+      "https://i.pinimg.com/564x/68/57/16/685716ef980cf439fac453064c57957f.jpg",
+  },
+  {
+    id: "4",
+    corte: "Barba degradê style",
+    valor: 20.0,
+    time: 30,
+    image:
+      "https://i.pinimg.com/564x/fd/b2/2c/fdb22cbb88361de6419482b5eab0fdc1.jpg",
+  },
+  {
+    id: "5",
+    corte: "Barba all top",
+    valor: 20.0,
+    time: 30,
+    image:
+      "https://i.pinimg.com/564x/87/49/6d/87496d9cf02ea48727f380830fb26793.jpg",
+  },
+  {
+    id: "5",
+    corte: "Barba boxed",
+    valor: 20.0,
+    time: 30,
+    image:
+      "https://i.pinimg.com/564x/67/ab/f8/67abf8f365da2ae75ca35c3586dfd736.jpg",
+  },
+];
