d6ebed6145af917907c735f7be4fc56a064e9d38
thomascostta <thomas.jeffcosta@gmail.com>
2022-03-30 15:55:45 -0300

    create Actions

--- src/store/modules/salao/actions.js
+++ src/store/modules/salao/actions.js
@@ -7,3 +7,15 @@ export function allServices(services) {
 export function updateForm(payload) {
   return { type: types.UPDATE_FORM, payload };
 }
+
+export function updateScheduling(payload) {
+  return { type: types.UPDATE_SCHEDULING, payload };
+}
+
+export function updateSchedule(payload) {
+  return { type: types.UPDATE_SCHEDULE, payload };
+}
+
+export function filterSchedule(payload) {
+  return { type: types.FILTER_SCHEDULE, payload };
+}
