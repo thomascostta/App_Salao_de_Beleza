905b46cbe7dc6633cd5b86d88c1ae0022287de8e
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-01 11:18:02 -0300

    settings

--- src/components/ModalScheduling/resume.js
+++ src/components/ModalScheduling/resume.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Image } from "react-native";
-import { Box, Title, Text, Cover, Spacer } from "../../styles";
+import { Box, Title, Text, Spacer } from "../../styles";
 import theme from "../../styles/theme.json";
 import util from "../../util";
 
-const Resume = ({ agendamento, services }) => {
-  const serviceArray = services.filter(
-    (item) => item.id === agendamento.servicoId
-  );
-  const [service] = serviceArray;
-
+const Resume = ({ service }) => {
   const currencyFormat = (num) => {
     return "R$ " + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   };
@@ -20,17 +15,11 @@ const Resume = ({ agendamento, services }) => {
       hasPadding
       background={util.toAlpha(theme.colors.muted, 5)}
     >
-      {/* <Cover
-        width="80px"
-        heigth="80px"
-        image="https://www.minhatatuagem.com/wp-content/uploads/2021/06/corte-degrade-masculino-2.jpg"
-      // Ajustar aqui
-      /> */}
       <Image
         source={{ uri: service.image }}
         style={{
           width: 70,
-          height: 70,
+          height: 80,
           borderRadius: 3,
           borderWidth: 2,
           marginHorizontal: 10,
