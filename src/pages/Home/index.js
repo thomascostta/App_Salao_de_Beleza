38d7237d5315632125818206baa4193b787142da
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-05 18:12:26 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,7 +37,7 @@ const Home = () => {
         keyExtractor={(item, index) => index.toString()}
       />
       <ModalScheduling />
-      {/* <ModalSpecialists /> */}
+      <ModalSpecialists />
     </>
   );
 };
