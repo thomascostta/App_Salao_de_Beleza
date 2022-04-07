bd6d7e28a348c453112ffaa7c7f13e35ce646941
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-07 18:11:44 -0300

    create payment method Array

---
+++ src/data/paymentMethod.js
@@ -0,0 +1,10 @@
+export const paymentByApp = [
+  { id: '1_App', name: "Cartão de Crédito", paymentLocation: "App" },
+  { id: '2_App', name: "Cartão de Débito", paymentLocation: "App" },
+  { id: '3_App', name: "PIX", paymentLocation: "App" }
+];
+
+export const paymentOnTheSpot = [
+  { id: '1_Spot', name: "Cartão de Débito ou Cŕedito", paymentLocation: "Spot" },
+  { id: '2_Spot', name: "Dinheiro", paymentLocation: "Spot" },
+];
