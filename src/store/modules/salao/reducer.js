90378b289b7f8f86fdd5a35bb659c0215922e510
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-07 18:27:25 -0300

    create payment Array

--- src/store/modules/salao/reducer.js
+++ src/store/modules/salao/reducer.js
@@ -21,6 +21,7 @@ const INITIAL_STATE = {
     inputFiltroFoco: false,
     modalEspecialista: false,
     modalAgendamento: false,
+    modalPayment: false,
     agendamentoLoading: false,
   },
 };
