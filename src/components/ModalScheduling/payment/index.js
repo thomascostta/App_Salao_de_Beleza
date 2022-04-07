3d58f3519a1d7a8d90bf9e7860f25b1ec0c3b016
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-07 18:12:18 -0300

    update

--- src/components/ModalScheduling/payment/index.js
+++ src/components/ModalScheduling/payment/index.js
@@ -4,11 +4,20 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import theme from "../../../styles/theme.json";
 import util from "../../../util";
 import { Text, Touchable } from "../../../styles";
+import { paymentByApp, paymentOnTheSpot } from "../../../data/paymentMethod";
+import { useDispatch } from "react-redux";
+import { updatePaymentMethod } from "../../../store/modules/salao/actions";
 import Checkout from "../checkout";
 
 const PaymentPicker = () => {
-  const [selectPayment, setSelectPayment] = useState("App_CredCard");
+  const dispatch = useDispatch();
+  const [selectPayment, setSelectPayment] = useState();
   console.log("selectPayment  ", selectPayment);
+
+  const selectPaymentMethod = (item) => {
+    dispatch(updatePaymentMethod(item));
+    setSelectPayment(item.id);
+  };
 
   return (
     <View style={{ paddingHorizontal: 20 }}>
@@ -22,110 +31,73 @@ const PaymentPicker = () => {
           Pague pelo App
         </Text>
       </View>
-      <View>
-        <View>
-          <Touchable
-            height="30px"
-            rounded="5px"
-            background={util.toAlpha(theme.colors.muted, 5)}
-            border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
-            align="center"
-            hasPadding
-            onPress={() => setSelectPayment("App_CredCard")}
-          >
-            <View
-              style={[
-                styles.botton,
-                {
-                  backgroundColor:
-                    selectPayment === "App_CredCard"
-                      ? theme.colors.success
-                      : theme.colors.light,
-                },
-              ]}
-            />
-            <Text spacing="5px">Cartão de Crédito</Text>
-          </Touchable>
-        </View>
+      <>
+        {paymentByApp.map((item) => {
+          return (
+            <View style={{ marginTop: 10 }} key={item.id}>
+              <Touchable
+                height="30px"
+                rounded="5px"
+                background={util.toAlpha(theme.colors.muted, 5)}
+                border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
+                align="center"
+                hasPadding
+                onPress={() => selectPaymentMethod(item)}
+              >
+                <View
+                  style={[
+                    styles.botton,
+                    {
+                      backgroundColor:
+                        selectPayment === item.id
+                          ? theme.colors.success
+                          : theme.colors.light,
+                    },
+                  ]}
+                />
+                <Text spacing="5px">{item.name}</Text>
+              </Touchable>
+            </View>
+          );
+        })}
+      </>
 
-        <View style={{ marginTop: 10 }}>
-          <Touchable
-            height="30px"
-            rounded="5px"
-            background={util.toAlpha(theme.colors.muted, 5)}
-            border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
-            align="center"
-            hasPadding
-            onPress={() => setSelectPayment("App_DebitCard")}
-          >
-            <View
-              style={[
-                styles.botton,
-                {
-                  backgroundColor:
-                    selectPayment === "App_DebitCard"
-                      ? theme.colors.success
-                      : theme.colors.light,
-                },
-              ]}
-            />
-            <Text spacing="5px">Cartão de Débito</Text>
-          </Touchable>
-        </View>
-        <View style={{ marginTop: 20 }}>
-          <Text bold color="dark">
-            Pague na hora
-          </Text>
-        </View>
-        <View>
-          <Touchable
-            height="30px"
-            rounded="5px"
-            background={util.toAlpha(theme.colors.muted, 5)}
-            border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
-            align="center"
-            hasPadding
-            onPress={() => setSelectPayment("Place_DebitCard")}
-          >
-            <View
-              style={[
-                styles.botton,
-                {
-                  backgroundColor:
-                    selectPayment === "Place_DebitCard"
-                      ? theme.colors.success
-                      : theme.colors.light,
-                },
-              ]}
-            />
-            <Text spacing="5px">Cartão de Débito ou Crédito</Text>
-          </Touchable>
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Touchable
-            height="30px"
-            rounded="5px"
-            background={util.toAlpha(theme.colors.muted, 5)}
-            border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
-            align="center"
-            hasPadding
-            onPress={() => setSelectPayment("Pay_Cash_On_Place")}
-          >
-            <View
-              style={[
-                styles.botton,
-                {
-                  backgroundColor:
-                    selectPayment === "Pay_Cash_On_Place"
-                      ? theme.colors.success
-                      : theme.colors.light,
-                },
-              ]}
-            />
-            <Text spacing="5px">Dinheiro</Text>
-          </Touchable>
-        </View>
+      <View style={{ marginTop: 20 }}>
+        <Text bold color="dark">
+          Pague na hora
+        </Text>
       </View>
+
+      <>
+        {paymentOnTheSpot.map((item) => {
+          return (
+            <View style={{ marginTop: 10 }} key={item.id}>
+              <Touchable
+                height="30px"
+                rounded="5px"
+                background={util.toAlpha(theme.colors.muted, 5)}
+                border={`1px solid ${util.toAlpha(theme.colors.muted, 40)}`}
+                align="center"
+                hasPadding
+                onPress={() => selectPaymentMethod(item)}
+              >
+                <View
+                  style={[
+                    styles.botton,
+                    {
+                      backgroundColor:
+                        selectPayment === item.id
+                          ? theme.colors.success
+                          : theme.colors.light,
+                    },
+                  ]}
+                />
+                <Text spacing="5px">{item.name}</Text>
+              </Touchable>
+            </View>
+          );
+        })}
+      </>
     </View>
   );
 };
