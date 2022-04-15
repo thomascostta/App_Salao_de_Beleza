d58adc58f3c6b875dd7591b143e76c59f29e0ffc
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-15 11:10:28 -0300

    reactotron config

--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -1,13 +1,13 @@
-import Reactotron from 'reactotron-react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { reactotronRedux } from 'reactotron-redux';;
+import Reactotron from "reactotron-react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { reactotronRedux } from "reactotron-redux";
 
 Reactotron
+  .useReactNative()
+  .configure({ host: '' })
   .setAsyncStorageHandler(AsyncStorage)
-  .configure() 
-  .useReactNative() 
   .use(reactotronRedux())
-  .connect()
+  .connect();
 
-  console.tron = Reactotron;
-  export default Reactotron;
+console.tron = Reactotron;
+export default Reactotron;
