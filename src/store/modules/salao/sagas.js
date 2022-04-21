7e5b077c88817e7ca16e761711c0c31e29ce3e31
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-21 17:32:50 -0300

    saga muted

--- src/store/modules/salao/sagas.js
+++ src/store/modules/salao/sagas.js
@@ -1,3 +1,21 @@
-import { all, takeLatest } from 'redux-saga/effects';
+// import { all, call, put, takeLatest } from "redux-saga/effects";
+// import api from "../../../services/api";
+// import consts from "../../../consts";
+// import { updateSalao } from "./actions";
+// import types from "./types";
 
-export default all([])
+// export function* getSalao() {
+//   try {
+//     const { data: res } = yield call(api.get, `/salao/${consts.salaoId}`);
+//     if (res.error) {
+//       alert(err.message);
+//       return false;
+//     }
+
+//     yield put(updateSalao(res.salao));
+//   } catch (err) {
+//     alert(err.message);
+//   }
+// }
+
+// export default all([takeLatest(types.GET_SALAO, getSalao)]);
