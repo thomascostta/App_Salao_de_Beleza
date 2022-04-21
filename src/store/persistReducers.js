0423a1ae017b7494ba9eee39ef9cb42ff775eab9
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-21 17:33:22 -0300

    create persistReducers

---
+++ src/store/persistReducers.js
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer } from "redux-persist";
+
+export default (reducers) => {
+  const persistedReducer = persistReducer(
+    {
+      key: "salao_de_beleza_barber_rock_bar",
+      storage: AsyncStorage,
+      whitelist: [],
+    },
+    reducers
+  );
+  return persistedReducer;
+};
